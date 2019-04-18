7846206e57d1e22bbf4cd3758e2b00c035935c7c
mayankmetha <mayankmethad@gmail.com>
2019-04-18 14:43:43 +0530

    added database initalization

---
+++ database.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import sqlite3
+
+db = "demo.db"
+conn = None
+c = None
+
+def connectDB(args):
+    global conn 
+    conn = sqlite3.connect(args)
+
+def closeDB():
+    global conn
+    conn.close()
+
+def query(args):
+    c.execute(args)
+    conn.commit()
+
+def initDB():
+    global conn, db, c
+    connectDB(db)
+    c = conn.cursor()
+    query("CREATE TABLE users (userid text,passwd text)")
+    query("INSERT INTO users VALUES ('mayank','mayank')")
+    closeDB()
+
+def execute(args):
+    global conn, db, c
+    connectDB(db)
+    c = conn.cursor()
+    query(args)
+    closeDB()
---
+++ logger.py
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+from termcolor import colored
+
+def star():
+    return colored(' ★ ','magenta',attrs=['bold','blink'])
+
+def tick():
+    return colored(' ✔ ','green',attrs=['bold','blink'])
+
+def cross():
+    return colored(' ✘ ','red',attrs=['bold','blink'])
+
+def level(x):
+    if x == 0:
+        return colored("Executing",'yellow',attrs=['bold'])
+    elif x == 1:
+        return colored("Executed ",'yellow',attrs=['bold'])
+    elif x == 2:
+        return colored("Failed   ",'yellow',attrs=['bold'])
+
+def sep():
+    return colored(':','white',attrs=['bold'])
+
+def msg(args):
+    return colored(args,'blue',attrs=['bold'])
